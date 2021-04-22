ffa4a419af83011018d655e9992816f9a1448243
Farida Alin <farida.alin@yahoo.com>
2021-04-22 20:51:57 +0200

    catchAsync handler created

--- server/routes/api/user.js
+++ server/routes/api/user.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const userController = require('../../controllers/user_controller');
+const validate = require('../../utils/validate');
+const userSchema = require('../../utils/validate');
+// console.log('validate:', validate);
+// console.log('userSchema:', userSchema);
 
 // /messages
 router.get('/', userController.all_users);
 
 // /users/register
-router.post('/register', userController.user_register);
+router.post(
+  '/register',
+  validate.validate(userSchema.userSchema),
+  userController.user_register
+);
 router.post('/login', userController.user_login);
 router.post('/change-password', userController.user_changePassword);
 
