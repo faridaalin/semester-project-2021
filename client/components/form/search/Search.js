9ec90f89676a0795f348b2edbbd4970f5596facd
Farida Alin <farida.alin@yahoo.com>
2021-04-29 13:51:59 +0200

    work in progress: home page

--- client/components/form/search/Search.js
+++ client/components/form/search/Search.js
@@ -4,26 +4,46 @@ import styles from './serach.module.css';
 const Search = () => {
   return (
     <form className={styles.form}>
-      <div>
+      <div className={styles.inputContainer}>
         <label htmlFor='search' className={styles.label}>
           <MapPin className={styles.icon} />
           Location
         </label>
-        <input type='search' name='search' id='serach' />
+        <input
+          type='search'
+          name='search'
+          id='serach'
+          placeholder='Where do you want to stay?'
+          className={styles.input}
+        />
       </div>
-      <div>
-        <label htmlFor='dates' className={styles.label}>
-          <Calendar className={styles.icon} />
-          Dates
-        </label>
-        <input type='date' name='dates' id='dates' />
-      </div>
-      <div>
-        <label htmlFor='guests' className={styles.label}>
-          <Users className={styles.icon} />
-          Guests
-        </label>
-        <input type='number' name='guests' id='guests' />
+      <div className={styles.column}>
+        <div>
+          <label htmlFor='dates' className={styles.label}>
+            <Calendar className={styles.icon} />
+            Dates
+          </label>
+          <input
+            type='date'
+            name='dates'
+            id='dates'
+            placeholder='Add dates'
+            className={styles.input}
+          />
+        </div>
+        <div>
+          <label htmlFor='guests' className={styles.label}>
+            <Users className={styles.icon} />
+            Guests
+          </label>
+          <input
+            type='number'
+            name='guests'
+            id='guests'
+            placeholder='Add guests'
+            className={styles.input}
+          />
+        </div>
       </div>
       <button>Search</button>
     </form>
