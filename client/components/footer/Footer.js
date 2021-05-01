6e8cc8a608005c613682bf0f53dfb1c6de8bc008
Farida Alin <farida.alin@yahoo.com>
2021-05-01 10:42:48 +0200

    footer styling

--- client/components/footer/Footer.js
+++ client/components/footer/Footer.js
@@ -5,49 +5,58 @@ import styles from './footer.module.css';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <section>
-        <div className={styles.logo}>
-          <span className={styles.logoLetter}>H</span>
-          <span className={styles.logoLetters}>olidaze</span>
-        </div>
-        <p>Håkonsgaten 15</p>
-        <p>Bergen, Norway 15</p>
-        <div className={styles.socialMedia}>
-          <Facebook />
-          <Instagram />
-          <Mail />
-        </div>
-      </section>
-      <section>
-        <h3>About Holizade</h3>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-        <Link href='/about'>
-          <a>About us</a>
-        </Link>
-        <Link href='/hotels'>
-          <a>Our Accomodations</a>
-        </Link>
-        <Link href='/contact'>
-          <a>Contact us</a>
-        </Link>
-      </section>
-      <section>
-        <h3>Help</h3>
-        <Link href=''>
-          <a>Our Policy</a>
-        </Link>
-        <Link href=''>
-          <a>Customer Service</a>
-        </Link>
-        <Link href=''>
-          <a>F&Q</a>
-        </Link>
-        <Link href=''>
-          <a>Payment Options</a>
-        </Link>
-      </section>
+      <div className={styles.innerFooter}>
+        <section>
+          <h3 className={styles.logo}>
+            <span className={styles.firstLetter}>H</span>
+            <span className={styles.logoLetters}>olidaze</span>
+          </h3>
+          <div className={styles.contentContainer}>
+            <p>Håkonsgaten 15</p>
+            <p>Bergen, Norway 15</p>
+            <div className={styles.socialMedia}>
+              <Facebook />
+              <Instagram />
+              <Mail />
+            </div>
+          </div>
+        </section>
+        <section>
+          <h3>About Holizade</h3>
+          <div className={styles.contentContainer}>
+            <Link href='/'>
+              <a>Home</a>
+            </Link>
+            <Link href='/about'>
+              <a>About us</a>
+            </Link>
+            <Link href='/hotels'>
+              <a>Our Accomodations</a>
+            </Link>
+            <Link href='/contact'>
+              <a>Contact us</a>
+            </Link>
+          </div>
+        </section>
+        <section>
+          <h3>Help</h3>
+          <div className={styles.contentContainer}>
+            <Link href=''>
+              <a>Our Policy</a>
+            </Link>
+            <Link href=''>
+              <a>Customer Service</a>
+            </Link>
+            <Link href=''>
+              <a>F&Q</a>
+            </Link>
+            <Link href=''>
+              <a>Payment Options</a>
+            </Link>
+          </div>
+        </section>
+      </div>
+
       <section className={styles.note}>
         <p>This website was created for educational purpose only</p>
       </section>
