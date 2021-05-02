da29887af10bc953cd65eb71e829ec8a28a560a0
Farida Alin <farida.alin@yahoo.com>
2021-05-02 18:19:36 +0200

    date formating

--- client/components/form/search/Search.js
+++ client/components/form/search/Search.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { MapPin, Calendar, Users } from 'react-feather';
+import moment from 'moment';
 import Button from '../../button/Button';
 import Guests from './guest/Guests';
 import styles from './search.module.css';
@@ -9,24 +10,35 @@ import 'react-date-range/dist/theme/default.css'; // theme css file
 import { X } from 'react-feather';
 
 const Search = () => {
-  const [showDates, setShowDates] = useState(false);
-  const [showGuests, setShowGuests] = useState(false);
-  const [dateRage, setDateRange] = useState([
+  const intitalDateRange = [
     {
       startDate: new Date(),
-      endDate: new Date(),
+      endDate: null,
       key: 'selection',
     },
-  ]);
+  ];
+  const [showGuests, setShowGuests] = useState(false);
+  const [calendar, setCalendar] = useState(false);
+  const [resetDateInput, setResetDateInput] = useState('Add dates');
+  const [dateRage, setDateRange] = useState(intitalDateRange);
 
   const handleSearch = (e) => {
     e.preventDefault();
   };
 
   const closeModal = () => {
-    setShowDates(false);
+    setCalendar(false);
   };
-  console.log('showDates', showDates);
+  const handleDateReset = () => {
+    setResetDateInput('Add dates');
+    setDateRange(intitalDateRange);
+  };
+
+  const formatDates = (startDate, endDate) => {
+    return `${moment(startDate).add(10, 'days').calendar()} - ${moment(endDate)
+      .add(10, 'days')
+      .calendar()}`;
+  };
 
   return (
     <form className={styles.form} onSubmit={handleSearch}>
@@ -52,16 +64,21 @@ const Search = () => {
 
           <input
             type='button'
-            value='Add dates'
+            value={
+              !dateRage[0].endDate
+                ? 'Add dates'
+                : `${formatDates(dateRage[0].startDate, dateRage[0].endDate)}`
+            }
             className={styles.inputButton}
-            onClick={() => setShowDates(!showDates)}
+            onClick={() => setCalendar(!calendar)}
           />
 
-          {showDates && (
+          {calendar && (
             <div className={styles.dateRange}>
               <button className={styles.closeModel}>
                 <X className={styles.closeicon} onClick={closeModal} />
               </button>
+              <button onClick={handleDateReset}>Clear</button>
               <DateRange
                 editableDateInputs={true}
                 onChange={(item) => setDateRange([item.selection])}
