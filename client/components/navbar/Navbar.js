836387fd15d2bbb4d920c4a4a0ad4184a6dcf6cd
Farida Alin <farida.alin@yahoo.com>
2021-05-02 19:56:35 +0200

    added acttive link

--- client/components/navbar/Navbar.js
+++ client/components/navbar/Navbar.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Button from '../button/Button';
 import styles from './navbar.module.css';
@@ -7,7 +8,7 @@ import { ChevronDown } from 'react-feather';
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
-
+  const router = useRouter();
   const toggleMenu = () => {
     setOpen(!open);
   };
@@ -23,7 +24,9 @@ const Navbar = () => {
     return () => {
       window.removeEventListener('resize', hidemenu);
     };
-  }, [open]);
+  }, []);
+
+  console.log(router.pathname);
 
   return (
     <header className={`${styles.container} ${open && styles.headerColor}`}>
@@ -49,22 +52,38 @@ const Navbar = () => {
 
       <nav className={`${styles.nav}  ${open ? styles.show : ''}`}>
         <ul className={`${styles.items}`}>
-          <li className={styles.item}>
+          <li
+            className={`${styles.item} ${
+              router.pathname === '/' && styles.active
+            } `}
+          >
             <Link href='/'>
               <a>Home</a>
             </Link>
           </li>
-          <li className={styles.item}>
+          <li
+            className={`${styles.item} ${
+              router.pathname === '/hotels' && styles.active
+            } `}
+          >
             <Link href='/hotels'>
               <a>Hotels</a>
             </Link>
           </li>
-          <li className={styles.item}>
+          <li
+            className={`${styles.item} ${
+              router.pathname === '/about' && styles.active
+            } `}
+          >
             <Link href='/about'>
               <a>About</a>
             </Link>
           </li>
-          <li className={styles.item}>
+          <li
+            className={`${styles.item} ${
+              router.pathname === '/contact' && styles.active
+            } `}
+          >
             <Link href='/contact'>
               <a>Contact</a>
             </Link>
--- client/pages/hotels.js
+++ client/pages/hotels.js
@@ -1,10 +1,5 @@
 import Layout from '../components/layout/Layout';
-import DesktopHero from '../components/desktopHero/DesktopHero';
 
 export default function Hotels() {
-  return (
-    <Layout title='Hotels'>
-      <DesktopHero />
-    </Layout>
-  );
+  return <Layout title='Hotels'>Hotels</Layout>;
 }
