1e75aa4e7403d94abd65f622228afc01944c41ee
Farida Alin <farida.alin@yahoo.com>
2021-05-06 19:23:31 +0200

    WIP: pill

--- client/components/pill/Pill.js
+++ client/components/pill/Pill.js
@@ -1,19 +1,36 @@
+import { useState } from 'react';
 import { ChevronDown } from 'react-feather';
 import styles from './pill.module.css';
 
 const Pill = ({ name, select }) => {
+  const [show, setShow] = useState(false);
   if (select < 2) {
     return <button className={`${styles.primaryButton}`}>{name}</button>;
   }
   return (
     <div className={styles.container}>
-      <button className={`${styles.primaryButton}`}>
+      <button
+        className={`${styles.primaryButton}`}
+        onClick={() => setShow(!show)}
+      >
         {name} <ChevronDown className={styles.icon} />
       </button>
-      <div className={styles.buttonContainer}>
-        <button className={styles.button}>{name}: Hight to low</button>
-        <button className={styles.button}>{name}: Low to heigh</button>
-      </div>
+      {show && (
+        <div className={styles.buttonContainer}>
+          <button
+            className={styles.button}
+            onClick={() => console.log('Sort hotels heigh to low')}
+          >
+            {name}: Hight to low
+          </button>
+          <button
+            className={styles.button}
+            onClick={() => console.log('Sort hotels low to heigh')}
+          >
+            {name}: Low to heigh
+          </button>
+        </div>
+      )}
     </div>
   );
 };
