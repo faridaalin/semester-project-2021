5a25f4daaeedff96a0464aff3d9ee0988c7e60ca
Farida Alin <farida.alin@yahoo.com>
2021-05-10 15:15:28 +0200

    dashboard styling :  WIP

--- client/components/accordion/Accordion.js
+++ client/components/accordion/Accordion.js
@@ -3,45 +3,48 @@ import dateFormat from 'dateformat';
 import styles from './accordion.module.css';
 
 const Accordion = ({ messages }) => {
-  const [open, setOpen] = useState('');
-  const [height, setHeight] = useState('0px');
+  const [open, setOpen] = useState(false);
+
   const content = useRef(null);
 
-  const toggleAccordion = () => {
-    setOpen(open === '' ? 'open' : '');
-    setHeight(open === 'open' ? '0px' : `${content.current.scrollHeight}px`);
-    console.log(content.current.scrollHeight);
+  const toggleAccordion = (index) => {
+    console.log('index:', index);
+    console.log('Open Toggle:', open);
+    if (open === index) {
+      return setOpen(null);
+    }
+    setOpen(index);
   };
+
+  console.log('Open OUTSIDE', open);
   return (
     <div className={styles.accordionSection}>
-      <dispatchEvent
-        className={`${styles.accordion}`}
-        onClick={toggleAccordion}
-      >
+      <dispatchEvent className={`${styles.accordion}`}>
         <span>From</span>
         <span>Subject</span>
         <span>Date</span>
         <span>Status</span>
       </dispatchEvent>
 
-      {messages?.data?.map((message) => (
+      {messages?.data?.map((message, index) => (
         <div className={styles.accordionContent} key={message._id}>
           <div
-            className={`${styles.accordionHeader} ${open ? styles.open : ''}`}
-            onClick={toggleAccordion}
+            className={`${styles.accordionHeader} ${
+              open === index ? styles.open : ''
+            }`}
+            onClick={() => toggleAccordion(index)}
+            key={index}
           >
             <span>{message.firstname}</span>
             <span>{message.subject}</span>
             <span>{dateFormat(`${message.createdAt}`, 'mm/dd/yyyy')}</span>
             <span>{message.isRead === false ? 'Unread' : 'Read'}</span>
           </div>
-          <div
-            className={styles.accordionText}
-            ref={content}
-            style={{ maxHeight: `${setHeight}` }}
-          >
-            <p>{message.message}</p>
-          </div>
+          {open === index ? (
+            <div className={styles.accordionText} ref={content}>
+              <p>{message.message}</p>
+            </div>
+          ) : null}
         </div>
       ))}
     </div>
