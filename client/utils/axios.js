c4d8b9eec2279e7031bb74b4a334d33151865c99
faridaalin <farida.alin@yahoo.com>
2021-05-20 18:06:47 +0200

    cors issues WIP

--- client/utils/axios.js
+++ client/utils/axios.js
@@ -3,7 +3,14 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_LOCAL_API,
   withCredentials: true,
+  responseType: 'json',
+  withCredentials: true,
+  timeout: 10000,
   credentials: 'include',
+  headers: {
+    'X-Requested-With': 'XMLHttpRequest',
+    'Content-Type': 'application/json',
+  },
 });
 
 export default instance;
