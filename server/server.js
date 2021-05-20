65601e974dc1f3122fc9a76eb65da3008c3df2b0
faridaalin <farida.alin@yahoo.com>
2021-05-20 14:31:05 +0200

    cors fixes

--- server/server.js
+++ server/server.js
@@ -38,8 +38,11 @@ mongoose.connect(
 
 // MIDDLEWARE
 // app.use(cors({ origin: '*', credentials: true }));
-// app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({ origin: 'https://final-2021-frontend.vercel.app', credentials: true })
+);
+// app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
