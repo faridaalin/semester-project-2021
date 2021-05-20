7fd2b13426286e25d6dd2b2bd33959d7fa29d3c7
faridaalin <farida.alin@yahoo.com>
2021-05-20 16:11:53 +0200

    cors issues WIP

--- server/server.js
+++ server/server.js
@@ -44,17 +44,17 @@ mongoose.connect(
 //   res.header('Access-Control-Allow-Origin', req.header('origin'));
 //   next();
 // });
-const allowlist = ['http://example1.com', 'http://example2.com']
-const corsOptionsDelegate = function (req, callback) {
-  const corsOptions;
+let allowlist = ['http://example1.com', 'http://example2.com'];
+let corsOptionsDelegate = function (req, callback) {
+  let corsOptions;
   if (allowlist.indexOf(req.header('Origin')) !== -1) {
-    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
   } else {
-    corsOptions = { origin: false } // disable CORS for this request
+    corsOptions = { origin: false }; // disable CORS for this request
   }
-  callback(null, corsOptions) // callback expects two parameters: error and options
-}
-app.use(cors(options));
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
