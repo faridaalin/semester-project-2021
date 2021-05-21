96da0e9e9fe4fd79fb81f0acef85c3327c61f032
faridaalin <farida.alin@yahoo.com>
2021-05-21 21:12:36 +0200

    focus state on buttons and cards

--- client/pages/404.js
+++ client/pages/404.js
@@ -4,14 +4,16 @@ import Layout from '@/components/layout/Layout';
 const NotFound = () => {
   return (
     <Layout>
-      <h1>Ooops...</h1>
-      <h2>That page cannot be found.</h2>
-      <p>
-        Go back to the{' '}
-        <Link href='/'>
-          <a>homepage</a>
-        </Link>
-      </p>
+      <section className='section'>
+        <h1>Ooops...</h1>
+        <h2>That page cannot be found.</h2>
+        <p>
+          Go back to the{' '}
+          <Link href='/'>
+            <a>homepage</a>
+          </Link>
+        </p>
+      </section>
     </Layout>
   );
 };
