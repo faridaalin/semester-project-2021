27851f6044520f54a2d650c98acdca35d98c09ad
faridaalin <farida.alin@yahoo.com>
2021-05-23 15:30:33 +0200

    WCAG

--- client/components/card/Card.js
+++ client/components/card/Card.js
@@ -10,48 +10,52 @@ const Card = (props) => {
 
   if (hotel) {
     return (
-      <Link href={`/hotel/${hotel._id}`}>
-        <a
-          className={styles.card}
-          style={{
-            backgroundImage: `url(${hotel.main_image})`,
-          }}
-        >
-          <div className={styles.content}>
-            <span>
-              {showRating(hotel.rating).map((i) => (
-                <Star key={i} className={styles.rating} />
-              ))}
-            </span>
-            <h3 className={`${styles.h3} ${styles.headerFavs}`}>
-              {hotel.title}
-            </h3>
-            <h4 className={styles.h4}>{hotel.address}</h4>
-            <div
-              className={styles.description}
-              dangerouslySetInnerHTML={{
-                __html: truncate(hotel.description, 120),
-              }}
-            ></div>
-          </div>
-        </a>
-      </Link>
+      <div>
+        <Link href={`/hotel/${hotel._id}`}>
+          <a
+            className={styles.card}
+            style={{
+              backgroundImage: `url(${hotel.main_image})`,
+            }}
+          >
+            <div className={styles.content}>
+              <span>
+                {showRating(hotel.rating).map((i) => (
+                  <Star key={i} className={styles.rating} />
+                ))}
+              </span>
+              <h3 className={`${styles.h3} ${styles.headerFavs}`}>
+                {hotel.title}
+              </h3>
+              <h4 className={styles.h4}>{hotel.address}</h4>
+              <div
+                className={styles.description}
+                dangerouslySetInnerHTML={{
+                  __html: truncate(hotel.description, 120),
+                }}
+              ></div>
+            </div>
+          </a>
+        </Link>
+      </div>
     );
   }
   return (
-    <Link href={`/category/${hotelStyle.slug}`}>
-      <a
-        className={styles.cardType}
-        style={{
-          backgroundImage: `url(${hotelStyle.imageurl})`,
-        }}
-        data-style={hotelStyle.slug}
-      >
-        <div className={styles.content}>
-          <h3 className={styles.h3}>{hotelStyle.type}</h3>
-        </div>
-      </a>
-    </Link>
+    <div className={styles.cardWrapper}>
+      <Link href={`/category/${hotelStyle.slug}`}>
+        <a
+          className={styles.cardType}
+          style={{
+            backgroundImage: `url(${hotelStyle.imageurl})`,
+          }}
+          data-style={hotelStyle.slug}
+        >
+          <div className={styles.content}>
+            <h3 className={styles.h3}>{hotelStyle.type}</h3>
+          </div>
+        </a>
+      </Link>
+    </div>
   );
 };
 
--- client/components/card/hotelCard/hotelCard.js
+++ client/components/card/hotelCard/hotelCard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Router from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -39,56 +39,58 @@ const HotelCard = ({ hotel, setItemTodelete, setModal }) => {
   );
 
   return (
-    <Link href={`/hotel/${hotel._id}`}>
-      <a>
-        <div className={styles.hotelCard}>
-          <div className={styles.imageContainer}>
-            <Image
-              src={hotel.main_image}
-              alt={hotel.title}
-              layout='fill'
-              objectFit='cover'
-              className={styles.image}
-            />
-          </div>
-
-          <div className={styles.content}>
-            <h3 className={`${styles.h3} ${styles.header}`}>{hotel.title}</h3>
-            <h4 className={styles.h4}>{hotel.address}</h4>
-            <span>
-              {showRating(hotel.rating).map((i) => (
-                <Star key={i} className={styles.rating} />
-              ))}
-            </span>
-            <div className={styles.description}>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: truncate(hotel.description, 110),
-                }}
+    <div>
+      <Link href={`/hotel/${hotel._id}`}>
+        <a>
+          <div className={styles.hotelCard}>
+            <div className={styles.imageContainer}>
+              <Image
+                src={hotel.main_image}
+                alt={hotel.title}
+                layout='fill'
+                objectFit='cover'
+                className={styles.image}
               />
             </div>
-            <div className={styles.buttonContainer}>
-              <Button color='orange' size='s'>
-                View More
-              </Button>
-              {admin === true && (
-                <div className={styles.icons}>
-                  <button
-                    className={styles.btnIcon}
-                    onClick={handleDeleteButton}
-                  >
-                    <Trash2 className={styles.iconDelete} />
-                  </button>
-                  <button className={styles.btnIcon} onClick={handleUpdate}>
-                    <Edit className={styles.iconUpdate} />
-                  </button>
-                </div>
-              )}
+
+            <div className={styles.content}>
+              <h3 className={`${styles.h3} ${styles.header}`}>{hotel.title}</h3>
+              <h4 className={styles.h4}>{hotel.address}</h4>
+              <span>
+                {showRating(hotel.rating).map((i) => (
+                  <Star key={i} className={styles.rating} />
+                ))}
+              </span>
+              <div className={styles.description}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: truncate(hotel.description, 110),
+                  }}
+                />
+              </div>
+              <div className={styles.buttonContainer}>
+                <Button color='orange' size='s'>
+                  View More
+                </Button>
+                {admin === true && (
+                  <div className={styles.icons}>
+                    <button
+                      className={styles.btnIcon}
+                      onClick={handleDeleteButton}
+                    >
+                      <Trash2 className={styles.iconDelete} />
+                    </button>
+                    <button className={styles.btnIcon} onClick={handleUpdate}>
+                      <Edit className={styles.iconUpdate} />
+                    </button>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-      </a>
-    </Link>
+        </a>
+      </Link>
+    </div>
   );
 };
 
